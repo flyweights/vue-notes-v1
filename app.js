e327781d0d80f7ade1efc22485179d5caf88979f
量飞 <flyweights@163.com>
2017-11-05 15:15:20 +0800

    修改笔记样式

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const Note = {
     template: `
-    <div>笔记</div>
+    <div class="item">
+        <div class="content">
+            <div class="header">笔记</div>
+        </div>
+    </div>
     `
 }
 
 const Notes = {
     components: {
-      'note': Note  
+        'note': Note
     },
     template: `
-    <div>
-        <a>添加笔记</a>
-        <note></note>
-        <note></note>
-        <note></note>
-        <note></note>        
+    <div class="ui container notes">
+        <h4 class="ui horizontal divider header">
+            <i class="paw icon"></i>
+            vue-notes-v1
+        </h4>
+        <a class="ui right floated basic violet button">添加笔记</a>
+        <div class="ui divided items">
+            <note></note>
+            <note></note>
+            <note></note>
+            <note></note>
+        </div>
     </div>
     `
 }
