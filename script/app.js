899edfbaaa922a3dfa3959174d74984a2806a24c
量飞 <flyweights@163.com>
2017-11-14 23:52:54 +0800

    修改了代码的格式

--- script/app.js
+++ script/app.js
@@ -1,21 +1,21 @@
 const Editor = {
-    props: [
-        'entityObject'
-    ],
-    data () {
-        return {
-            entity: this.entityObject
-        }
-    },
-    //$emit未懂，回调给该父组件
-    methods: {
-        //回调更新
-        update() {
-            this.$emit("update")
-        },
-    },
-    //v-model的理解不够深
-    template:`
+  props: [
+    'entityObject'
+  ],
+  data () {
+    return {
+      entity: this.entityObject
+    }
+  },
+  // $emit未懂，回调给该父组件
+  methods: {
+    // 回调更新
+    update () {
+      this.$emit('update')
+    }
+  },
+  // v-model的理解不够深
+  template: `
         <div class="ui form">
             <div class="field">
                 <textarea
@@ -30,46 +30,50 @@ const Editor = {
 }
 
 const Note = {
-    props: [
-        'entityObject',
-    ],
-    data () {
-        return {
-            entity: this.entityObject,
-            open: false
-        }
+  // 相爱
+  props: [
+    'entityObject'
+  ],
+  data () {
+    return {
+      entity: this.entityObject,
+      open: false
+    }
+  },
+  // 相杀
+  computed: {
+    // 计算属性
+    header () {
+      return _.truncate(this.entity.body, {
+        length: 30
+      })
     },
-    //计算属性
-    computed: {
-        header(){
-            return _.truncate(this.entity.body, {length:30})
-        },
-        updated () {
-            return moment(this.entity.meta.updated).fromNow()
-        },
-        words() {
-            //去掉空格
-            return this.entity.body.trim().length
-        }
+    updated () {
+      return moment(this.entity.meta.updated).fromNow()
     },
-    methods: {
-      save() {
-        loadCollection('notes')
-            .then((collection) => {
-                collection.update(this.entity)
-                db.saveDatabase()
-            })
-      },
-      destroy () {
-          this.$emit('destroy', this.entity.$loki)
-      } 
+    words () {
+      // 去掉空格
+      return this.entity.body.trim().length
+    }
+  },
+  methods: {
+    save () {
+      loadCollection('notes')
+        .then((collection) => {
+          collection.update(this.entity)
+          db.saveDatabase()
+        })
     },
-    //键是注册的标签名字，值是组件变量名
-    components: {
-      'editor': Editor  
-    },
-    //v-bind父子传值
-    template: `
+    destroy () {
+      this.$emit('destroy', this.entity.$loki)
+    }
+  },
+  // 键是注册的标签名字，值是组件变量名
+  components: {
+    'editor': Editor
+  },
+  // v-bind父子传值
+  template: `
     <div class="item">
         <div class="meta">
             {{ updated }}
@@ -96,55 +100,57 @@ const Note = {
 }
 
 const Notes = {
-    data () {
-        return {
-            entities: []
-        }
-    },
-    methods: {
-      create() {
-          loadCollection('notes')
-            .then((collection) => {
-                const entity = collection.insert({
-                    body: ''
-                })
-                db.saveDatabase()//保存到数据库
-                this.entities.unshift(entity)//修改界面
-            })
-      },
-      destroy (id) {
-          const _entities = this.entities.filter((entity) => {
-              return entity.$loki !== id
+  data () {
+    return {
+      entities: []
+    }
+  },
+  methods: {
+    create () {
+      loadCollection('notes')
+        .then((collection) => {
+          const entity = collection.insert({
+            body: ''
           })
-          this.entities = _entities
+          db.saveDatabase() // 保存到数据库
+          this.entities.unshift(entity) // 修改界面
+        })
+    },
+    destroy (id) {
+      const _entities = this.entities.filter((entity) => {
+        return entity.$loki !== id
+      })
+      this.entities = _entities
 
-          loadCollection('notes')
-            .then((collection) => {
-                collection.remove({ '$loki':id })
-                db.saveDatabase()
-            })
-      }
-    },
-    created () {
-        loadCollection('notes')
-            .then((collection) => {
-                //console.log(collection)
-                const _entities = collection.chain()//链操作
-                    .find()//取出
-                    .simplesort('$loki', 'isdesc')//排序
-                    .data()//返回数组
-                this.entities = _entities
-                console.log(this.entities + this.key)
-            })
-    },
-    components: {
-        'note': Note
-    },
-    /*
-        v-bind的理解不足
-        暂时的理解是，让界面某一字符串跟着变量走
-    */
-    template: `
+      loadCollection('notes')
+        .then((collection) => {
+          collection.remove({
+            '$loki': id
+          })
+          db.saveDatabase()
+        })
+    }
+  },
+  created () {
+    loadCollection('notes')
+      .then((collection) => {
+        // console.log(collection)
+        const _entities = collection.chain() // 链操作
+          .find() // 取出
+          .simplesort('$loki', 'isdesc') // 排序
+          .data() // 返回数组
+        this.entities = _entities
+        console.log(this.entities + this.key)
+      })
+  },
+  components: {
+    'note': Note
+  },
+  /*
+      v-bind的理解不足
+      暂时的理解是，让界面某一字符串跟着变量走
+  */
+  template: `
     <div class="ui container notes">
         <h4 class="ui horizontal divider header">
             <i class="paw icon"></i>
@@ -163,24 +169,24 @@ const Notes = {
             <span
                 class="ui small disabled header"
                 v-if="!this.entities.length">
-                还没有笔记，情点击“添加笔记”按钮
+                还没有笔记，请点击“添加笔记”按钮
             </span>
         </div>
     </div>
     `
-    
+
 }
 
 /**
- * Vue最简单的用法就是e，c，t，m，p ...
- * 常用函数d，
+ * Vue最简单的用法就是e，c，t ...
+ * 常用函数d，相爱pd，相杀cm
  */
 const app = new Vue({
-    el: '#app',
-    components: {
-        'notes': Notes
-    },
-    template: `
+  el: '#app',
+  components: {
+    'notes': Notes
+  },
+  template: `
         <notes></notes>
     `
 })
--- script/db.js
+++ script/db.js
@@ -1,24 +1,24 @@
 const db = new loki('notes', {
-    autoload: true,
-    autoloadCallback: databaseInitialize,
-    autosave: true,
-    autosaveInterval: 3000
+  autoload: true,
+  autoloadCallback: databaseInitialize,
+  autosave: true,
+  autosaveInterval: 3000
 })
 
-function databaseInitialize() {
-    const notes = db.getCollection('notes')
-    if (notes === null) {
-        db.addCollection('notes')
-    }
+function databaseInitialize () {
+  const notes = db.getCollection('notes')
+  if (notes === null) {
+    db.addCollection('notes')
+  }
 }
 
-function loadCollection(collection) {
-    return new Promise(
-        resolve => {
-            db.loadDatabase({}, () => {
-                const _collection = db.getCollection(collection) || db.addCollection('notes')
-                resolve(_collection)
-            })
+function loadCollection (collection) {
+  return new Promise(
+    resolve => {
+      db.loadDatabase({}, () => {
+        const _collection = db.getCollection(collection) || db.addCollection('notes')
+        resolve(_collection)
+      })
     })
 }
 
